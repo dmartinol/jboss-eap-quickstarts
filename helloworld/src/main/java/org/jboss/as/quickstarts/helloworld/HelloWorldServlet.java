119fe9b04a900c7ac7d6d9253d703bdbfebf2ea7
Daniele Martinoli <dmartino@redhat.com>
2022-09-05 12:32:30 +0200

    Added HELLO_NAME parameter

--- helloworld/src/main/java/org/jboss/as/quickstarts/helloworld/HelloWorldServlet.java
+++ helloworld/src/main/java/org/jboss/as/quickstarts/helloworld/HelloWorldServlet.java
@@ -52,10 +52,11 @@ public class HelloWorldServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = System.getProperty("HELLO_NAME", "World");
         resp.setContentType("text/html");
         PrintWriter writer = resp.getWriter();
         writer.println(PAGE_HEADER);
-        writer.println("<h1>" + helloService.createHelloMessage("World") + "</h1>");
+        writer.println("<h1>" + helloService.createHelloMessage(name) + "</h1>");
         writer.println(PAGE_FOOTER);
         writer.close();
     }
